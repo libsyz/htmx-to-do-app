b19499037d4b08e57d81bad66100a07c4584e241
miguel jimenez <denkatalabeno@gmail.com>
2020-06-02 14:29:28 +0800

    implemented struct for tasks

--- task_repo.rb
+++ task_repo.rb
@@ -1,12 +1,32 @@
 
+class Task < Struct.new(:description, :done)
+    def initialize(description, done = false)
+      super
+    end
+
+    def mark_as_done!
+      done = true
+    end
+
+    def done?
+      done
+    end
+end
+
 class TaskRepo
   def initialize
-    @tasks = ['do the laundry', 'do the dishes']
+    @tasks = []
+    seed
   end
 
   def all
     @tasks
   end
 
+  private
+
+  def seed
+    @tasks << Task.new("Do the dishes") << Task.new("Do the laundry")
+  end
 
 end
