c07e6e711e329448e5b88ad71544548f12a2e6fd
miguel jimenez <denkatalabeno@gmail.com>
2020-06-05 16:17:16 +0800

    demo part is feature complete

--- app.rb
+++ app.rb
@@ -1,70 +1,68 @@
 require 'sinatra'
 require_relative "task_repo"
 require_relative "fighter_repo"
-
 require 'pry-byebug'
 
+use Rack::MethodOverride
 
-  use Rack::MethodOverride
-
-  class Sinatra::Application
-    def initialize
-      @tasks = TaskRepo.new
-      @fighters = FighterRepo.new
-      super
-    end
+class Sinatra::Application
+  def initialize
+    @tasks = TaskRepo.new
+    @fighters = FighterRepo.new
+    super
   end
+end
 
-  get '/' do
-    erb :tasks, layout: :index
-  end
+get '/' do
+  erb :tasks, layout: :index
+end
 
-  post '/tasks' do
-    unless params['task'].empty?
-      @tasks.all << Task.new(params['task'])
-      erb :tasklist
-    else
-      erb :validation_error
-    end
-  end
-
-  delete '/tasks/:id' do
-    @tasks.all.delete_at(params[:id].to_i)
+post '/tasks' do
+  unless params['task'].empty?
+    @tasks.all << Task.new(params['task'])
     erb :tasklist
+  else
+    erb :validation_error
   end
+end
 
-  patch '/tasks/:id' do
-    @tasks.all[params[:id].to_i].mark_as_done!
-    erb :tasklist
-  end
+delete '/tasks/:id' do
+  @tasks.all.delete_at(params[:id].to_i)
+  erb :tasklist
+end
+
+patch '/tasks/:id' do
+  @tasks.all[params[:id].to_i].mark_as_done!
+  erb :tasklist
+end
 
 # Just Demo Purpose
 
-  get '/demo' do
-    erb :demo
-  end
+get '/demo' do
+  erb :demo
+end
 
-  get '/fighters' do
-    @selected = @fighters.all.find { |el| el[:name] == params['fighter']}
-    erb :fighter_selected
-  end
+get '/fighters' do
+  @selected = @fighters.all.find { |el| el[:name] == params['fighter']}
+  erb :fighter_selected
+end
 
-  get '/techniques' do
-    @special = params[:technique]
-    binding.pry
-    case @special
-      when "hadouken" || "tiger_shot"
-        erb :'moves/hadouken'
-      when "tiger_knee"
-        erb :'moves/tiger_knee'
-      when "shoryuken"
-        erb :'moves/shoryuken'
-      when "kikoken"
-        erb :'moves/kikoken'
-      when "renkiko"
-        erb :'moves/renkiko'
-      end
+get '/techniques' do
+  @special = params[:technique]
+  case @special
+  when "hadouken", "tiger_shot"
+    @special = special.split("_").map(&:capitalize).join(" ")
+    erb :'moves/hadouken'
+  when "tiger_knee"
+    erb :'moves/tiger_knee'
+  when "shoryuken"
+    erb :'moves/shoryuken'
+  when "kikoken"
+    erb :'moves/kikoken'
+  when "renkiko"
+    erb :'moves/renkiko'
   end
+end
 
 
 
